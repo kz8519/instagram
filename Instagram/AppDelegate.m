b4ae659b5cc2fded790fa6ccbfc8a7c8ba3c0f86
Kathy Zhong <kzhong@fb.com>
2022-06-27 13:25:52 -0700

    added icon + launch screen

--- Instagram/AppDelegate.m
+++ Instagram/AppDelegate.m
@@ -28,6 +28,21 @@
 
     [Parse initializeWithConfiguration:config];
     
+    /*
+    // Testing Parse Integration
+    PFObject *gameScore = [PFObject objectWithClassName:@"GameScore"];
+    gameScore[@"score"] = @1337;
+    gameScore[@"playerName"] = @"Sean Plott";
+    gameScore[@"cheatMode"] = @NO;
+    [gameScore saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+        if (succeeded) {
+            NSLog(@"Object saved!");
+        } else {
+            NSLog(@"Error: %@", error.description);
+        }
+    }];
+     */
+    
     return YES;
 }
 
