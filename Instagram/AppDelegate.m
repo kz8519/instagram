0b0281aef5fc1a13a239503799ad8796a6d8f24d
Kathy Zhong <kzhong@fb.com>
2022-06-27 13:10:19 -0700

    deployed and configured Parse server

--- Instagram/AppDelegate.m
+++ Instagram/AppDelegate.m
@@ -6,6 +6,7 @@
 //
 
 #import "AppDelegate.h"
+#import "Parse/Parse.h"
 
 @interface AppDelegate ()
 
@@ -15,7 +16,18 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    
     // Override point for customization after application launch.
+    
+    ParseClientConfiguration *config = [ParseClientConfiguration  configurationWithBlock:^(id<ParseMutableClientConfiguration> configuration) {
+
+        configuration.applicationId = @"leFOcQArzZUbARUlqsaaOB1Of4Xsvxr5vpRQ3uEY";
+        configuration.clientKey = @"Jq8SYPLtX9YZ7PGJLhJVtSPGxUVTkEhJekqIlQu6";
+        configuration.server = @"https://parseapi.back4app.com";
+    }];
+
+    [Parse initializeWithConfiguration:config];
+    
     return YES;
 }
 
