ff02f6277f9efcba2460093154871433d22e42dd
Kathy Zhong <kzhong@fb.com>
2022-06-30 10:02:52 -0700

    users can edit bio

--- Instagram/View Controllers/ProfileViewController.m
+++ Instagram/View Controllers/ProfileViewController.m
@@ -10,13 +10,19 @@
 #import "DetailsViewController.h"
 #import "Post.h"
 @import Parse;
+@import UITextView_Placeholder;
+
 
 @interface ProfileViewController ()<UICollectionViewDataSource, UICollectionViewDelegate>
 @property (strong, nonatomic) IBOutlet PFImageView *profileImageView;
+//@property (strong, nonatomic) IBOutlet UITextView *bioView;
+@property (strong, nonatomic) IBOutlet UILabel *usernameLabel;
+@property (strong, nonatomic) IBOutlet UILabel *bioLabel;
 - (IBAction)changeProfilePhoto:(id)sender;
 @property (strong, nonatomic) IBOutlet UICollectionView *collectionView;
 //@property (strong, nonatomic) IBOutlet UICollectionViewFlowLayout *flowLayout;
 @property (nonatomic, strong) NSMutableArray *arrayOfPosts;
+- (IBAction)didTapEdit:(id)sender;
 
 
 @end
@@ -29,11 +35,19 @@
     self.collectionView.delegate = self;
     self.collectionView.dataSource = self;
     
+//    self.bioView.placeholder = @"Add bio here";
+    
     PFUser *user = [PFUser currentUser];
     if (user[@"profilePicture"] != nil) {
         self.profileImageView.file = user[@"profilePicture"];
         [self.profileImageView loadInBackground];
     }
+    
+    if (user[@"bio"] != nil) {
+        self.bioLabel.text = user[@"bio"];
+    }
+    
+    self.usernameLabel.text = user.username;
     
     [self queryPosts:user];
 }
@@ -142,6 +156,34 @@
     UIGraphicsEndImageContext();
     
     return newImage;
+}
+
+- (IBAction)didTapEdit:(id)sender {
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Edit Biography" message:@"Input your new biography below" preferredStyle:(UIAlertControllerStyleAlert)];
+    
+    [alert addTextFieldWithConfigurationHandler:^(UITextField * _Nonnull textField) {
+        textField.placeholder = @"Enter new biography";
+    }];
+    
+    // create an Save action
+    UIAlertAction *saveAction = [UIAlertAction actionWithTitle:@"Save" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        // handle response here.
+        PFUser *currentUser = [PFUser currentUser];
+        currentUser[@"bio"] = [[alert textFields][0] text];
+        [currentUser saveInBackgroundWithBlock:^(BOOL succeeded, NSError * _Nullable error) {
+            if (succeeded) {
+                NSLog(@"New bio saved!");
+                self.bioLabel.text = currentUser[@"bio"];
+            } else {
+                NSLog(@"Error: %@", error.description);
+            }
+        }];
+    }];
+    
+    // add the Save action to the alert controller
+    [alert addAction:saveAction];
+    
+    [self presentViewController:alert animated:YES completion:nil];
 }
 
 @end
