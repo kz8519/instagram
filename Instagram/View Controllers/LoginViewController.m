235df3f351313ac570358d96933e00d268395395
Kathy Zhong <kzhong@fb.com>
2022-06-30 14:17:02 -0700

    refactored login view controller

--- Instagram/View Controllers/LoginViewController.m
+++ Instagram/View Controllers/LoginViewController.m
@@ -5,8 +5,8 @@
 //  Created by Kathy Zhong on 6/27/22.
 //
 
-#import "LoginViewController.h"
 #import <Parse/Parse.h>
+#import "LoginViewController.h"
 #import "FeedViewController.h"
 
 @interface LoginViewController ()
@@ -34,88 +34,68 @@
 }
 */
 
+- (BOOL)fieldIsEmpty {
+    return ([self.usernameField.text isEqual:@""] || [self.passwordField.text isEqual:@""]);
+}
+
+- (void)emptyFieldAlert {
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Empty Fields" message:@"There are empty fields" preferredStyle:(UIAlertControllerStyleAlert)];
+    
+    // Create an OK action
+    UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil];
+    
+    // Add the OK action to the alert controller
+    [alert addAction:okAction];
+    
+    [self presentViewController:alert animated:YES completion:nil];
+}
+
+- (void)parseSignup: (PFUser *)user {
+    // call sign up function on the object
+    [user signUpInBackgroundWithBlock:^(BOOL succeeded, NSError * error) {
+        if (error != nil) {
+            NSLog(@"Error: %@", error.localizedDescription);
+        }
+        else {
+            NSLog(@"User registered successfully");
+            
+            // Swap window root view controller
+            UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+            FeedViewController *feedNVC = [storyboard instantiateViewControllerWithIdentifier:@"TabBarController"];
+            self.view.window.rootViewController = feedNVC;
+        }
+    }];
+}
+
+- (void)parseLogin: (NSString *)username: (NSString *)password {
+    [PFUser logInWithUsernameInBackground:username password:password block:^(PFUser * user, NSError *  error) {
+        if (error != nil) {
+            NSLog(@"User log in failed: %@", error.localizedDescription);
+        } else {
+            NSLog(@"User logged in successfully");
+            
+            // Swap window root view controller
+            UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+            FeedViewController *feedNVC = [storyboard instantiateViewControllerWithIdentifier:@"TabBarController"];
+            self.view.window.rootViewController = feedNVC;
+        }
+    }];
+}
+
 - (IBAction)registerUser:(id)sender {
-    // initialize a user object
+    // Initialize a user object
     PFUser *newUser = [PFUser user];
     
-    // set user properties
+    // Set user properties
     newUser.username = self.usernameField.text;
     newUser.password = self.passwordField.text;
     
-    // If the username or password field is empty, alert the user
-    if ([self.usernameField.text isEqual:@""] || [self.passwordField.text isEqual:@""]) {
-        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Empty Fields" message:@"There are empty fields" preferredStyle:(UIAlertControllerStyleAlert)];
-        
-        // create an OK action
-        UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
-            // handle response here.
-        }];
-        
-        // add the OK action to the alert controller
-        [alert addAction:okAction];
-        
-        [self presentViewController:alert animated:YES completion:^{
-            // optional code for what happens after the alert controller has finished presenting
-        }];
-    }
-    else { // Otherwise, call sign-up function on the object
-        // call sign up function on the object
-        [newUser signUpInBackgroundWithBlock:^(BOOL succeeded, NSError * error) {
-            if (error != nil) {
-                NSLog(@"Error: %@", error.localizedDescription);
-            } else {
-                NSLog(@"User registered successfully");
-                
-                // manually segue to logged in view
-//                [self performSegueWithIdentifier:@"loginSegue" sender:nil];
-                // Swap window root view controller
-                UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
-                FeedViewController *feedNVC = [storyboard instantiateViewControllerWithIdentifier:@"TabBarController"];
-                self.view.window.rootViewController = feedNVC;
-            }
-        }];
-    }
+    [self fieldIsEmpty] ? [self emptyFieldAlert] : [self parseSignup:newUser];
 }
 
 - (IBAction)loginUser:(id)sender {
-    NSString *username = self.usernameField.text;
-    NSString *password = self.passwordField.text;
     
-    // If the username or password field is empty, alert the user
-    if ([self.usernameField.text isEqual:@""] || [self.passwordField.text isEqual:@""]) {
-        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Empty Fields" message:@"There are empty fields" preferredStyle:(UIAlertControllerStyleAlert)];
-        
-        // create an OK action
-        UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
-            // handle response here.
-        }];
-        
-        // add the OK action to the alert controller
-        [alert addAction:okAction];
-        
-        [self presentViewController:alert animated:YES completion:^{
-            // optional code for what happens after the alert controller has finished presenting
-        }];
-    }
-    else { // Otherwise, log the user in
-        [PFUser logInWithUsernameInBackground:username password:password block:^(PFUser * user, NSError *  error) {
-            if (error != nil) {
-                NSLog(@"User log in failed: %@", error.localizedDescription);
-            } else {
-                NSLog(@"User logged in successfully");
-                
-                // display view controller that needs to shown after successful login
-//                [self performSegueWithIdentifier:@"loginSegue" sender:nil];
-                
-                
-                // TODO: sync login with logout (these three lines are what logout uses; either change login to this, or change logout to the one-line dismiss view) (same deal above)
-                // Swap window root view controller
-                UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
-                FeedViewController *feedNVC = [storyboard instantiateViewControllerWithIdentifier:@"TabBarController"];
-                self.view.window.rootViewController = feedNVC;
-                
-            }
-        }];
-    }
+    [self fieldIsEmpty] ? [self emptyFieldAlert] : [self parseLogin: self.usernameField.text :self.passwordField.text];
+    
 }
 @end
